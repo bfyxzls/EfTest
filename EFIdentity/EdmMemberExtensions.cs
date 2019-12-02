537728214a6ebf5ca23987ea8e56969d47633b48
zhangzhanling <zhangzhanling@chinalawinfo.com>
2019-12-02 13:51:47 +0800

    添加实现

---
+++ EFIdentity/EdmMemberExtensions.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Metadata.Edm;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFIdentity
+{
+    /// <summary>
+    /// 获取自增的扩展方法
+    /// </summary>
+    public static class EdmMemberExtensions
+    {
+        public static StoreGeneratedPattern StoreGeneratedPattern2(this EdmMember @this)
+        {
+            const string name = "http://schemas.microsoft.com/ado/2009/02/edm/annotation:StoreGeneratedPattern";
+            var metaDataProperty = @this.MetadataProperties.FirstOrDefault(m => m.Name == name);
+
+            if (metaDataProperty == null)
+            {
+                return StoreGeneratedPattern.None;
+            }
+
+            return (StoreGeneratedPattern)Enum.Parse(typeof(StoreGeneratedPattern), (string)metaDataProperty.Value);
+        }
+
+        public static bool IsStoreGeneratedIdentity2(EdmMember @this)
+        {
+            return StoreGeneratedPattern2(@this) == StoreGeneratedPattern.Identity;
+        }
+
+        public static bool IsStoreGeneratedComputed2(EdmMember @this)
+        {
+            return StoreGeneratedPattern2(@this) == StoreGeneratedPattern.Computed;
+        }
+    }
+}
--- EFIdentity/Program.cs
+++ EFIdentity/Program.cs
@@ -1,15 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Data.EntityClient;
+using System.Data.Metadata.Edm;
+using System.Data.Objects;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace EFIdentity
 {
+   
     class Program
     {
         static void Main(string[] args)
         {
+            DbContext context = (DbContext)new Database1Entities();
+            ObjectContext objectContext = ((IObjectContextAdapter)context).ObjectContext;
+
+            MetadataWorkspace metadataWorkspace =
+     ((IObjectContextAdapter)context).ObjectContext.MetadataWorkspace;
+            ReadOnlyCollection<EdmType> edmTypes =
+                metadataWorkspace.GetItems<EdmType>(DataSpace.OSpace);
+
+
+
+            var entityTypes = metadataWorkspace.GetItems<EntityType>(DataSpace.CSpace);
+            var properties = entityTypes.SelectMany(type => type.Properties);
+            foreach (var item in properties)
+            {
+                Console.WriteLine(item.Name + "是否自增" + item.StoreGeneratedPattern2());
+            }
+            Console.ReadKey();
+
+
+
         }
     }
 }
