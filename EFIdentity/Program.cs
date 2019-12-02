966c9f9f5ba9988b5e354102118b16d11c31cd22
bfyxzls <bfyxzls@sina.com>
2019-12-02 22:53:55 +0800

    add

--- EFIdentity/Program.cs
+++ EFIdentity/Program.cs
@@ -12,27 +12,35 @@ using System.Threading.Tasks;
 
 namespace EFIdentity
 {
-   
+
     class Program
     {
-        static void Main(string[] args)
+        /// <summary>
+        /// 某个实体表是否存在自增键
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        static bool IsHasIdentityKey<T>(DbContext context)
         {
-            DbContext context = (DbContext)new Database1Entities();
             ObjectContext objectContext = ((IObjectContextAdapter)context).ObjectContext;
 
             MetadataWorkspace metadataWorkspace =
      ((IObjectContextAdapter)context).ObjectContext.MetadataWorkspace;
-            ReadOnlyCollection<EdmType> edmTypes =
-                metadataWorkspace.GetItems<EdmType>(DataSpace.OSpace);
+            var entityType = metadataWorkspace.GetItems<EntityType>(DataSpace.CSpace)
+                .FirstOrDefault(i => i.Name == typeof(T).Name);
+            var properties = entityType.Properties.Where(
+                i => EdmMemberExtensions.StoreGeneratedPattern3(i) == StoreGeneratedPattern.Identity);
+            return properties != null && properties.Any();
 
+        }
 
+        static void Main(string[] args)
+        {
+            DbContext context = (DbContext)new Database1Entities();
 
-            var entityTypes = metadataWorkspace.GetItems<EntityType>(DataSpace.CSpace);
-            var properties = entityTypes.SelectMany(type => type.Properties);
-            foreach (var item in properties)
-            {
-                Console.WriteLine(item.Name + "是否自增" + EdmMemberExtensions.StoreGeneratedPattern3(item));
-            }
+            Console.WriteLine("User是否自增" + IsHasIdentityKey<User>(context));
+
             Console.ReadKey();
 
 
